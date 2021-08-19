d447073a4dc6e0f4106a3da8676c61ab72f69549
igorocky <123@gmail.com>
2021-08-19 10:46:52 +0300

    Add few new methods to Boundaries.

--- src/main/kotlin/svgTypes.kt
+++ src/main/kotlin/svgTypes.kt
@@ -2,7 +2,7 @@ package org.igye.svg
 
 class Point(val x: Double, val y: Double)
 
-class Boundaries(val minX: Double, val maxX: Double, val minY: Double, val maxY: Double) {
+data class Boundaries(val minX: Double, val maxX: Double, val minY: Double, val maxY: Double) {
     init {
         if (!(minX <= maxX && minY <= maxY)) {
             throw SvgException("!(minX <= maxX && minY <= maxY)")
@@ -11,6 +11,20 @@ class Boundaries(val minX: Double, val maxX: Double, val minY: Double, val maxY:
 
     val width by lazy { maxX - minX }
     val height by lazy { maxY - minY }
+
+    fun addPoint(p: Point): Boundaries = Boundaries(
+        minX = if (minX <= p.x) minX else p.x,
+        maxX = if (p.x <= maxX) maxX else p.x,
+        minY = if (minY <= p.y) minY else p.y,
+        maxY = if (p.y <= maxY) maxY else p.y,
+    )
+
+    fun addAbsoluteMargin(m: Double): Boundaries = Boundaries(
+        minX = minX - m,
+        maxX = maxX + m,
+        minY = minY - m,
+        maxY = maxY + m,
+    )
 
     companion object {
         fun fromPoints(points: List<Point>): Boundaries {
